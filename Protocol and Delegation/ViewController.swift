c1278dff28e22ced817effcc5e7e3ea3706498f4
Ammy <ammypandey@ammys-macbook-air.local>
2018-01-30 19:28:03 +0530

    Initial Commit

---
+++ Protocol and Delegation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Protocol and Delegation
+//
+//  Created by Ammy Pandey on 30/01/18.
+//  Copyright © 2018 Ammy Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
